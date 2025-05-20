7410767b6b3207cd4484287ac4cdbec784524d5e
ed cuss <edcussmusic@gmail.com>
2025-05-20 21:09:30 +0100

    feat: fake fs struct

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,33 @@
-use sha2::{Digest, Sha256};
+use hash_files::{FakeFileSystem, FileSystem};
 use std::collections::HashMap;
-use std::env;
-use std::fmt;
-use std::fs;
-use std::io;
-use std::io::Read;
-use std::path::Path;
-use text_colorizer::*;
-use walkdir;
+use std::path::{Path, PathBuf};
+use std::{env, fmt};
+use text_colorizer::Colorize;
 
 fn main() {
     let args = Args::new();
-    process(args, list_files, hash_filestream)
-}
 
-fn process(
-    args: Args,
-    list_fn: fn(&String) -> Box<dyn Iterator<Item = walkdir::Result<walkdir::DirEntry>>>,
-    hash_fn: fn(&Path) -> io::Result<String>,
-) {
-    let hashes: HashMap<String, Vec<String>> = list_fn(&args.root_dir)
-        .filter_map(Result::ok)
-        .filter(|e| e.file_type().is_file())
-        .filter_map(|file| {
-            let path = file.path();
-            hash_fn(path)
-                .ok()
-                .map(|hash| (hash, path.display().to_string()))
-        })
-        .fold(HashMap::new(), |mut acc, (hash, path)| {
-            acc.entry(hash).or_default().push(path);
-            acc
-        });
+    let mut fake_fs = FakeFileSystem::new();
 
-    dbg!(hashes);
-}
+    let values = vec![
+        ("dir/file_1.txt", "hello"),
+        ("dir/file_2.txt", "hello"),
+        ("dir/file_3.txt", "world"),
+    ];
 
-fn list_files(path: &String) -> Box<dyn Iterator<Item = walkdir::Result<walkdir::DirEntry>>> {
-    Box::new(walkdir::WalkDir::new(path).into_iter())
-}
-
-fn read_file(path: &Path) -> Result<Vec<u8>, io::Error> {
-    let bytes: Vec<u8> = fs::read(path)?;
-    Ok(bytes)
-}
-
-fn hash_bytes(bytes: &Vec<u8>) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(bytes);
-    let result = hasher.finalize();
-    format!("{:x}", result)
-}
-
-fn hash_filestream(path: &Path) -> io::Result<String> {
-    let file = fs::File::open(path)?;
-    let mut reader = io::BufReader::new(file);
-    let mut hasher = Sha256::new();
-    let mut buffer = [0u8; 8192]; // 8KB
-
-    loop {
-        let count = reader.read(&mut buffer)?;
-        if count == 0 {
-            break;
-        }
-        hasher.update(&buffer[..count]);
+    for (name, content) in values {
+        fake_fs.files.insert(name.into(), content.to_string());
     }
 
-    let result = hasher.finalize();
-    Ok(format!("{:x}", result))
+    let mut hashes: HashMap<String, Vec<String>> = HashMap::new();
+    let paths: Vec<_> = fake_fs.list_files(Path::new(&args.root_dir)).collect();
+    for file in paths {
+        hashes
+            .entry(fake_fs.hash_file(&file).unwrap())
+            .or_default()
+            .push(file.display().to_string());
+    }
+    dbg!(hashes);
 }
 
 fn print_type_of<T>(_: &T) {
@@ -105,19 +65,5 @@ impl Args {
 impl fmt::Display for Args {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "root_dir: {}", self.root_dir)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hash_bytes() {
-        let inp_vec = vec![0, 1, 2, 3, 4];
-        let expected_result =
-            String::from("08bb5e5d6eaac1049ede0893d30ed022b1a4d9b5b48db414871f51c9cb35283d");
-
-        assert_eq!(hash_bytes(&inp_vec), expected_result);
     }
 }
